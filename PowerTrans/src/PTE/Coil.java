672050718506a9cc429ae3402e43f87bc7df7876
MrSoul <mikalai.turyshau@gmail.com>
2014-01-08 16:18:16 +0300

    start add entities

---
+++ PowerTrans/src/PTE/Coil.java
@@ -0,0 +1,11 @@
+package PTE;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mrsoul
+ * Date: 1/8/14
+ * Time: 4:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Coil {
+}
