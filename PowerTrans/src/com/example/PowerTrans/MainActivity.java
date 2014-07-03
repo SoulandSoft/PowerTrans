1427f43dc2d5f1f46cc18e4cf7d4c7d11c9b4ad9
MrSoul <mikalai.turyshau@gmail.com>
2014-07-04 00:35:17 +0300

    Continue refactoring, Result activity\fragment started.

--- PowerTrans/src/com/example/PowerTrans/MainActivity.java
+++ PowerTrans/src/com/example/PowerTrans/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.PowerTrans;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -9,61 +8,60 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends FragmentActivity {
 
 
     public LayoutInflater lInflater;
-    public MainControls mc_fragment;
+    public MainFragment mainFragment;
     final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-        lInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (savedInstanceState != null) {
-        mc_fragment = (MainControls) getSupportFragmentManager().findFragmentById(R.id.mc_fragment);
-    }
-       // else {
-       // myFragment = new MainControls();
-       // getSupportFragmentManager().beginTransaction().add(R.id.layout1, myFragment).commit();
-       // }
+            mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
+        }
+        // else {
+        // myFragment = new MainFragment();
+        // getSupportFragmentManager().beginTransaction().add(R.id.layout1, myFragment).commit();
+        // }
 
     }
 
-    protected void onResume(){
-         super.onResume();
-      mc_fragment  = (MainControls)getSupportFragmentManager().findFragmentById(R.id.mc_fragment);
-      Log.d(LOG_TAG, "On resume");
-     }
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Some changes
-		getMenuInflater().inflate(R.menu.main, menu);
-    	return true;
-	}
+    protected void onResume() {
+        super.onResume();
+        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
+        Log.d(LOG_TAG, "On resume");
+    }
 
     @Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle presses on the action bar items
-	    switch (item.getItemId()) {
-	        case R.id.item2 :
-	          //  sendMessage(this.getCurrentFocus());
-	            return true;
-	      //  case R.id.action_settings:
-	        //    openSettings();
-	         //   return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Some changes
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-    public void MainControls_Click(View view){
-        Log.d(LOG_TAG, "My fragment click call");
-        mc_fragment.Click(view);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.item2:
+                //  sendMessage(this.getCurrentFocus());
+                return true;
+            //  case R.id.action_settings:
+            //    openSettings();
+            //   return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void MainControls_Click(View view) {
+        Log.d(LOG_TAG, "MainFragment fragment click");
+        mainFragment.Click(view);
     }
 
 
