7c1cd88c5016e053d417d3cc8cf57e9e7f79d978
MrSoul <mikalai.turyshau@gmail.com>
2014-07-06 00:38:46 +0300

    continue display result

--- PowerTrans/src/com/example/PowerTrans/ResultFragment.java
+++ PowerTrans/src/com/example/PowerTrans/ResultFragment.java
@@ -1,9 +1,11 @@
 package com.example.PowerTrans;
 
+import PTE.Coil;
 import PTE.PowerTransformer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,9 +38,19 @@ public class ResultFragment extends Fragment {
     public void Click(View view) {
     }
 
+    // Method, that display formatted data.
     public void display(PowerTransformer pt) {
-        TextView T = (TextView) lLayout.findViewById(R.id.textView1);
-        T.setText("Power is " + pt.get_Power());
+        for (Coil c : pt.secondaries) {
+            Log.d(LOG_TAG, "Output Wire");
+            LinearLayout L = (LinearLayout) lInflater.inflate(R.layout.secondary_coil_output, null);
+            LinearLayout In = (LinearLayout) lLayout.findViewById(R.id.resultFragment);
+            TextView V = (TextView) L.findViewById(R.id.Voltage_output);
+            V.setText(" " + c.getVoltage());
+            TextView C = (TextView) L.findViewById(R.id.Current_output);
+            C.setText(" " + c.getCurrent());
+            In.addView(L);
+        }
+
 
     }
 }
